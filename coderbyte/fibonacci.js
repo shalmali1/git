3a7b7c5b7cf0340d9d8bf8c5b9194d1021680499
shalmali1 <p.shalmlai1@gmail.com>
2015-03-15 15:39:29 -0700

    Commit initial

---
+++ coderbyte/fibonacci.js
@@ -0,0 +1,22 @@
+//Have the function FibonacciChecker(num) return the string yes if the number given is part of the Fibonacci sequence. This sequence is defined by: Fn = Fn-1 + Fn-2, which means to find Fn you add the previous two numbers up. The first two numbers are 0 and 1, then comes 1, 2, 3, 5 etc. If num is not in the Fibonacci sequence, return the string no. 
+
+function FibonacciChecker(num) { 
+  var prev = 0;
+  var cur  = 1;
+  var next = 0;
+  
+  while (num > next) {
+  	next = cur + prev;
+	prev = cur;
+	cur = next;
+  };
+
+  //We will come here only if num == next OR next > num
+  if (num == next)
+    return "yes";
+  if (next > num)
+    return "no";
+}
+   
+
+FibonacciChecker(readline());           
---
+++ coderbyte/pallindrome.js
@@ -0,0 +1,23 @@
+//Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
+
+
+function reverseString(str) {
+  var rev = "";
+  length = str.length;
+  for (var j = length - 1; j >= 0 ; j--) {
+  	rev = rev + str.charAt(j);
+  }
+  return rev;    
+}
+
+
+function Palindrome(str) {
+  var rev = reverseString(str);
+  if (rev == str)
+  	return "true"
+  else
+    return "false";
+}
+   
+
+Palindrome(readline());           
