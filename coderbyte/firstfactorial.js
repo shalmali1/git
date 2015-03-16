76158ea9ddff7eec3183b3a1507767856a0fd33e
shalmali1 <p.shalmlai1@gmail.com>
2015-03-15 18:08:02 -0700

    Factorial program

---
+++ coderbyte/firstfactorial.js
@@ -0,0 +1,13 @@
+function FirstFactorial(num) { 
+
+  var prod = 1;
+  	for ( var i = 1 ; i <= num; i++)
+         { 
+          prod = prod * i;
+         }
+  return prod;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(4);                            
